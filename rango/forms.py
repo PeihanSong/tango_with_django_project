aa6e04bd51266b76b11b92371b57a7b72ea1d1b3
Peihan Song <2703170s@student.gla.ac.uk>
2022-02-10 17:03:50 +0000

    My 8th commit

--- rango/forms.py
+++ rango/forms.py
@@ -3,7 +3,8 @@ from rango.models import Page, Category
 
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
+    NAME_MAX_LENGTH = 128
+    name = forms.CharField(max_length=NAME_MAX_LENGTH, help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput, required=False)
@@ -14,8 +15,10 @@ class CategoryForm(forms.ModelForm):
 
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
-    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
+    TITLE_MAX_LENGTH = 128
+    URL_MAX_LENGTH = 200
+    title = forms.CharField(max_length=TITLE_MAX_LENGTH, help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=URL_MAX_LENGTH, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
